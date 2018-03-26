c3cb2b275efd10734d23a193b2a49e48cd33ffdf
sau <akinaysau@gmail.com>
2018-03-26 07:32:50 +0300

    ExceptionHelper.php

--- media/com_asm/src/Admin/Helpers/ExceptionHelper.php
+++ media/com_asm/src/Admin/Helpers/ExceptionHelper.php
@@ -7,18 +7,18 @@
  * @license     A "Slug" license name e.g. GPL2
  */
 
-namespace Sau\Joomla\ASM;
+namespace Sau\Joomla\ASM\Admin\Helpers;
 
 use Exception;
 
-class Helper {
-	public static function renderException ( Exception $e ) {
+abstract class ExceptionHelper {
+	public static function renderException( Exception $e ) {
 		echo '<pre>';
 		echo '<strong>Message:</strong> ', $e->getMessage(), PHP_EOL;
 		echo '<strong>Line:</strong> ', $e->getLine(), PHP_EOL;
 		echo '<strong>File:</strong> ', $e->getFile(), PHP_EOL;
 		echo '<strong>Trace:</strong> ', PHP_EOL;
-		print_r($e->getTrace());
+		print_r( $e->getTrace() );
 		echo '</pre>';
 	}
 }
